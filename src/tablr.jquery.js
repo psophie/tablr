0687dc437213e6c6c9e3db8178facafab5a703e4
Wanderson <wanderson.regis@ymail.com>
2013-02-21 09:03:46 -0300

    Starting

---
+++ src/tablr.jquery.js
@@ -0,0 +1,15 @@
+"use strict";
+
+(function(w, $)
+{
+    w.Tablr = function(/* Object */ options)
+    {
+        this.options = $.extend({}, options);
+        
+        // TODO
+    };
+
+    w.Tablr.prototype = {
+        // TODO
+    };
+})(window, jQuery);
---
+++ src/tablr.mootools.js
@@ -0,0 +1,19 @@
+"use strict";
+
+(function(w)
+{
+    w.Tablr = new Class({
+        Implements: Options,
+
+        options: {},
+
+        initialize: function(/* Object */ options)
+        {
+            this.setOptions(options);
+            
+            // TODO
+        }
+
+        // TODO
+    });
+})(window);
